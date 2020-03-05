b9c60d21c030fdec5bdd099db73725c4d3a44402
Ole Ersoy <ole.ersoy@gmail.com>
2020-03-05 06:50:20 -0600

    Made modifiers optional in matches

--- projects/validatorjs/src/lib/validators/matches.ts
+++ projects/validatorjs/src/lib/validators/matches.ts
@@ -4,10 +4,10 @@ import { assertString } from '../util/assertString';
  * Check whether the target matches the pattern
  * @param target The target
  * @param pattern The RegExp
- * @param modifiers The modifier
+ * @param modifiers The optional modifier
  * @return true if the target matches, false otherwise
  */
-export function matches(target:string, pattern:RegExp, modifiers:string) {
+export function matches(target:string, pattern:RegExp, modifiers?:string) {
   assertString(target);
   if (Object.prototype.toString.call(pattern) !== '[object RegExp]') {
     pattern = new RegExp(pattern, modifiers);
